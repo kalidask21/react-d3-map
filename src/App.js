e1a66f837835591e6113c87f70601fb5591b27a8
Kalidas <kalidas@github.com>
2020-06-07 19:17:34 -0500

    d3 Map Changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import CongressDistrict from './congressdistrict'
+
+class App extends Component {
+  render() {
+    return (
+       <svg width="960" height="600">
+        <CongressDistrict width="960" height="600" />
+       </svg>
+    );
+  }
+}
+
+export default App;
